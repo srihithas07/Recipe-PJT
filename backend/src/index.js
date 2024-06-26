42323888bce7eef12255b7d80d5d434009aa5fe2
srihithas07 <134121789+srihithas07@users.noreply.github.com>
2024-06-26 20:50:03 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,81 @@
+//import express from "express"
+//import cors from "cors"
+//import "dotenv/config"
+//import * as RecipeAPI from "./recipe-api"
+//import { PrismaClient } from "@prisma/client"
+
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const RecipeAPI = require("./recipe-api");
+const { PrismaClient } = require("@prisma/client");
+
+const app = express()
+const prismaClient = new PrismaClient()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/recipes/search", async (req, res) => {
+  const searchTerm = req.query.searchTerm
+  const page = parseInt(req.query.page)
+  const results = await RecipeAPI.searchRecipes(searchTerm, page)
+
+  return res.json(results)
+})
+
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+  const recipeId = req.params.recipeId
+  const results = await RecipeAPI.getRecipeSummary(recipeId)
+  return res.json(results)
+})
+
+app.post("/api/recipes/favourite", async (req, res) => {
+  const recipeId = req.body.recipeId
+
+  try {
+    const favouriteRecipe = await prismaClient.favouriteRecipes.create({
+      data: {
+        recipeId: recipeId
+      }
+    })
+    return res.status(201).json(favouriteRecipe)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: "Oops, something went wrong" })
+  }
+})
+
+app.get("/api/recipes/favourite", async (req, res) => {
+  try {
+    const recipes = await prismaClient.favouriteRecipes.findMany()
+    const recipeIds = recipes.map(recipe => recipe.recipeId.toString())
+
+    const favourites = await RecipeAPI.getFavouriteRecipesByIDs(recipeIds)
+
+    return res.json(favourites)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: "Oops, something went wrong" })
+  }
+})
+
+app.delete("/api/recipes/favourite", async (req, res) => {
+  const recipeId = req.body.recipeId
+
+  try {
+    await prismaClient.favouriteRecipes.delete({
+      where: {
+        recipeId: recipeId
+      }
+    })
+    return res.status(204).send()
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: "Oops, something went wrong" })
+  }
+})
+
+app.listen(5000, () => {
+  console.log("server running on localhost:5000")
+})
---
+++ backend/src/recipe-api.js
@@ -0,0 +1,65 @@
+const apiKey = process.env.API_KEY
+
+ const searchRecipes = async (searchTerm, page) => {
+  if (!apiKey) {
+    throw new Error("API Key not found")
+  }
+
+  const url = new URL("https://api.spoonacular.com/recipes/complexSearch")
+
+  const queryParams = {
+    apiKey,
+    query: searchTerm,
+    number: "10",
+    offset: (page * 10).toString()
+  }
+  url.search = new URLSearchParams(queryParams).toString()
+
+  try {
+    const searchResponse = await fetch(url)
+    const resultsJson = await searchResponse.json()
+    return resultsJson
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+ const getRecipeSummary = async recipeId => {
+  if (!apiKey) {
+    throw new Error("API Key not found")
+  }
+
+  const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/summary`)
+  const params = {
+    apiKey: apiKey
+  }
+  url.search = new URLSearchParams(params).toString()
+
+  const response = await fetch(url)
+  const json = await response.json()
+
+  return json
+}
+
+ const getFavouriteRecipesByIDs = async ids => {
+  if (!apiKey) {
+    throw new Error("API Key not found")
+  }
+
+  const url = new URL("https://api.spoonacular.com/recipes/informationBulk")
+  const params = {
+    apiKey: apiKey,
+    ids: ids.join(",")
+  }
+  url.search = new URLSearchParams(params).toString()
+
+  const searchResponse = await fetch(url)
+  const json = await searchResponse.json()
+
+  return { results: json }
+}
+
+
+module.exports.searchRecipes = searchRecipes;
+module.exports.getRecipeSummary = getRecipeSummary;
+module.exports.getFavouriteRecipesByIDs = getFavouriteRecipesByIDs;
